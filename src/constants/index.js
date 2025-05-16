920fbb35f9236a5de8e2601d0115b5a72ed2b2be
silasljennings <silasljennings@gmail.com>
2025-05-16 13:46:00 -0500

    added language for my work experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -58,19 +58,19 @@ export const clientReviews = [
 
 export const myProjects = [
   {
-    title: 'Podcastr - AI Podcast Platform',
-    desc: 'Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
+    title: 'Cashboard App - Geolocation Digital Rewards System',
+    desc: 'Cashboard App is a new and innovative way to connect vendors to consumers via digital rewards. It allows brands to configure and gamify the delivery of digital offers trough geo lotteries as well as event based scenarios.',
     subdesc:
-      'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
-    href: 'https://www.youtube.com/watch?v=zfAb95tJvZQ',
-    texture: '/textures/project/project1.mp4',
+      'Built as a consumer phone app with SwiftUI and Firebase is designed for user reach. Vendor platform built with Angular, Typescript, Firebase and GCP for max performance and scalability. This application is approaching launch.',
+    href: 'https://www.cashboardapp.co',
+    texture: '/textures/project/project1.mp4', //TODO:
     logo: '/assets/project-logo1.png',
     logoStyle: {
       backgroundColor: '#2A1816',
       border: '0.2px solid #36201D',
       boxShadow: '0px 0px 60px 0px #AA3C304D',
     },
-    spotlight: '/assets/spotlight1.png',
+    spotlight: '/assets/spotlight1.png', //TODO:
     tags: [
       {
         id: 1,
@@ -88,18 +88,18 @@ export const myProjects = [
         path: '/assets/typescript.png',
       },
       {
-        id: 4,
-        name: 'Framer Motion',
-        path: '/assets/framer.png',
+        id: 3,
+        name: 'Firebase',
+        path: '/assets/typescript.png', //TODO:
       },
     ],
   },
   {
-    title: 'LiveDoc - Real-Time Google Docs Clone',
-    desc: 'LiveDoc is a powerful collaborative app that elevates the capabilities of real-time document editing. As an enhanced version of Google Docs, It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.',
+    title: 'Dirty Santa Gift - Christmas Ecommerce platform',
+    desc: 'dirtysantagift.com is a seasonal ecommerce platform centered around providing awesome dirty/secret santa gifts.',
     subdesc:
-      'With LiveDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.',
-    href: 'https://www.youtube.com/watch?v=y5vE8y_f_OM',
+      'Built as a web application with React, Tailwind CSS, TypeScript, and Firebase Functions. Currently in active development.',
+    href: 'https://www.dirtysantagift.com',
     texture: '/textures/project/project2.mp4',
     logo: '/assets/project-logo2.png',
     logoStyle: {
@@ -124,19 +124,14 @@ export const myProjects = [
         name: 'TypeScript',
         path: '/assets/typescript.png',
       },
-      {
-        id: 4,
-        name: 'Framer Motion',
-        path: '/assets/framer.png',
-      },
     ],
   },
   {
-    title: 'CarePulse - Health Management System',
-    desc: 'An innovative healthcare platform designed to streamline essential medical processes. It simplifies patient registration, appointment scheduling, and medical record management, providing a seamless experience for both healthcare providers and patients.',
+    title: 'Dev Converters - All in one file and type converter',
+    desc: 'devconverters.com is your go-to tool for quick data formatting and conversions. Whether you\'re working with timestamps, numbers, or other developer assets, this tool simplifies the process in just one field and one click.',
     subdesc:
-      'With a focus on efficiency, CarePulse integrantes complex forms and SMS notifications, by using Next.js, Appwrite, Twillio and Sentry that enhance operational workflows.',
-    href: 'https://www.youtube.com/watch?v=lEflo_sc82g',
+      'Built as a web application with React, Tailwind CSS, and TypeScript. This application is approaching maturity.',
+    href: 'https://www.devconverters.com',
     texture: '/textures/project/project3.mp4',
     logo: '/assets/project-logo3.png',
     logoStyle: {
@@ -163,19 +158,14 @@ export const myProjects = [
         name: 'TypeScript',
         path: '/assets/typescript.png',
       },
-      {
-        id: 4,
-        name: 'Framer Motion',
-        path: '/assets/framer.png',
-      },
     ],
   },
   {
-    title: 'Horizon - Online Banking Platform',
-    desc: 'Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.',
+    title: 'Dev Deviants - Personal Brand',
+    desc: 'devdeviants.com is my personal brand where I host my tech blog and the portfolio you are currently viewing.',
     subdesc:
-      'Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.',
-    href: 'https://www.youtube.com/watch?v=PuOVqP_cjkE',
+      'Built as a web application with React, Vite, Tailwind CSS, TypeScript, and Jekyll. This application is ongoing and actively being updated as I grow my experience through my work and interactions.',
+    href: 'https://www.devdeviants.com',
     texture: '/textures/project/project4.mp4',
     logo: '/assets/project-logo4.png',
     logoStyle: {
@@ -192,54 +182,22 @@ export const myProjects = [
       },
       {
         id: 2,
-        name: 'TailwindCSS',
-        path: 'assets/tailwindcss.png',
-      },
-      {
-        id: 3,
-        name: 'TypeScript',
-        path: '/assets/typescript.png',
-      },
-      {
-        id: 4,
-        name: 'Framer Motion',
-        path: '/assets/framer.png',
-      },
-    ],
-  },
-  {
-    title: 'Imaginify - AI Photo Manipulation App',
-    desc: 'Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.',
-    subdesc:
-      'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
-    href: 'https://www.youtube.com/watch?v=Ahwoks_dawU',
-    texture: '/textures/project/project5.mp4',
-    logo: '/assets/project-logo5.png',
-    logoStyle: {
-      backgroundColor: '#1C1A43',
-      border: '0.2px solid #252262',
-      boxShadow: '0px 0px 60px 0px #635BFF4D',
-    },
-    spotlight: '/assets/spotlight5.png',
-    tags: [
-      {
-        id: 1,
-        name: 'React.js',
+        name: 'Vite',
         path: '/assets/react.svg',
       },
       {
-        id: 2,
+        id: 3,
         name: 'TailwindCSS',
         path: 'assets/tailwindcss.png',
       },
       {
-        id: 3,
+        id: 4,
         name: 'TypeScript',
         path: '/assets/typescript.png',
       },
       {
-        id: 4,
-        name: 'Framer Motion',
+        id: 5,
+        name: 'Jekyll',
         path: '/assets/framer.png',
       },
     ],
@@ -260,29 +218,38 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
 export const workExperiences = [
   {
     id: 1,
-    name: 'Framer',
-    pos: 'Lead Web Developer',
+    name: 'Altec Industries, Inc',
+    pos: 'Senior Software Developer',
     duration: '2022 - Present',
-    title: "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
-    icon: '/assets/framer.svg',
+    icon: '/assets/framer.svg', //TODO:
+    title: "Engineered AWS- and IoT-enabled, containerized microservices with Angular/Spring Boot UIs and Docker/Python tooling to process, analyze, and optimize ETL for over a billion telematics signals.",
     animation: 'victory',
   },
   {
     id: 2,
-    name: 'Figma',
-    pos: 'Web Developer',
+    name: 'Cent Holdings, LLC',
+    pos: 'Full Stack Software Engineer',
     duration: '2020 - 2022',
-    title: "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
-    icon: '/assets/figma.svg',
+    title: "Spearheaded the end-to-end development of a high-performance financial analytics platform—integrating private cloud Kubernetes clusters, Bloomberg Terminal, Wolfram notebooks, and VMWare virtualization—to process sub-second market data for 500+ securities while delivering custom user management and story-driven product requirements",
+    icon: '/assets/figma.svg', //TODO:
     animation: 'clapping',
   },
   {
     id: 3,
-    name: 'Notion',
-    pos: 'Junior Web Developer',
+    name: 'Intuitive Research & Technology',
+    pos: 'Software Development Engineer',
     duration: '2019 - 2020',
-    title: "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
-    icon: '/assets/notion.svg',
+    title: "Developed and maintained the Aviation Mission Planning System (AMPS) using C#, .NET, and Node.js to deliver secure, combat-ready pre-flight planning and in-flight analysis tools for US Army Blackhawk and Chinook helicopter pilots.",
+    icon: '/assets/notion.svg', //TODO:
     animation: 'salute',
+  },
+  {
+    id: 4,
+    name: 'NASA',
+    pos: 'Software Developer Intern',
+    duration: '2018 - 2018',
+    title: "Developed Advanced Concepts Office plugins that automated cross-disciplinary data aggregation—partnering with propulsion, aerospace, materials, and rocket engineers—to streamline project workflows and save management 10 hours per sprint.",
+    icon: '/assets/notion.svg', //TODO:
+    animation: 'clapping',
   },
 ];
